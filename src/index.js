0cfc9eaecd3b87d9c7f8c8b581c1f482fdb3fadb
SergeyKosovich <kosovichsergey322@gmail.com>
2021-02-27 17:51:48 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(a,b){
+  let e=[];
+  b.forEach(item=>e.push(item[0]+item[1]));
+  let g=a;
+  for(let i=0; i<=a.length; i++){
+      e.forEach( function(item){
+        if( g.includes(item) ){ g=g.split(item).join('') }
+      })
+  }
+  
+  if(g.length===0){return(true)}else{return(false)}
+   
+  }
